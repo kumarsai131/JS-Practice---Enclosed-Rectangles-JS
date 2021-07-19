1abf9f18372e1b9400e46b1d2cacb6fffa26f5ea
Sai Kumar <56537473+kumarsai131@users.noreply.github.com>
2021-07-19 21:58:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+RectangleA {
+top:'20px',
+left:'20px',
+height:'40px',
+width:'60px',
+children:[]
+};
+RectangleB{
+top:'30px',
+left:'30px',
+height:'20px',
+width:'30px',
+children:[]
+};
+
+
 function generateCodeFromObject(obj){
-    //return a code generated string
-   }
+    {
+            top:'20px',
+            left:'20px',
+            height:'40px',
+            width:'60px',
+            children:[{
+                top:'10px',
+                left:'10px',
+                height:'20px',
+                width:'30px',
+                children:[]
+            };
    
 module.exports=generateCodeFromObject;
 
