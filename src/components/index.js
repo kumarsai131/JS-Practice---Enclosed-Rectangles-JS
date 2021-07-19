39ea17feffd4c54020cd1b7db8926b0b5e250c3a
Sai Kumar <56537473+kumarsai131@users.noreply.github.com>
2021-07-19 22:59:07 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -7,9 +7,12 @@
 //}
 
 function updateStructure(rec1,rec2){
-	if(rec1.top == rec2.top && rec1.left==rec2.left && rec1.width == rec2.width && rec1.height = rec2.height)
-		return {rec1.top,rec1.left,rec1.width,rec1.height, children:[{rec2.top,rec2.left,rec2.width,rec2.height, children:[]}]};
-	
+	if(rec1.top+rec1.height > rec2.top+rec2.height && rec1.left+rec1.width > rec2.left+rec2.width && rec1.top<rec2.top && rec1.left<rec2.left)
+		return {top: rec1.top, left: rec1.left, height: rec1.height, width: rec1.width, children:[{top:rec2.top,left:rec2.left,height:rec2.height,width:rec2.width, children:[]}]};
+	else if(rec1.top+rec1.height == rec2.top+rec2.height && rec1.left+rec2.width == rec2.left+rec2.height && rec1.top=rec2.top && rec1.left=rec2.left)
+		return {top: rec2.top, left: rec2.left, height: rec2.height, width: rec2.width, children:[{top:rec1.top,left:rec1.left,height:rec1.height,width:rec1.width, children:[]}]};
+	else
+		return {top: rec1.top, left: rec1.left, height: rec1.height, width: rec1.width, children:[]}
 }
 
 module.exports = updateStructure;
