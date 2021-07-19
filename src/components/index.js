d1e8e0be862128e030236a70ea719878fc906155
Sai Kumar <56537473+kumarsai131@users.noreply.github.com>
2021-07-19 22:46:56 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,9 @@
 //}
 
 function updateStructure(rec1,rec2){
-	//write your code
+	if(rec1.top == rec2.top && rec1.left==rec2.left && rec1.width == rec2.width && rec1.height = rec2.height)
+		return {rec1.top,rec1.left,rec1.width,rec1.height, children:[{rec2.top,rec2.left,rec2.width,rec2.height, children:[]}]};
+	
 }
 
 module.exports = updateStructure;
