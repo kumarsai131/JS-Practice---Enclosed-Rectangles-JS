5bfc179d665e0fdabfcf309318b6e3aa9ab44030
Sai Kumar <56537473+kumarsai131@users.noreply.github.com>
2021-07-20 18:22:47 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -9,11 +9,10 @@
 function updateStructure(rec1,rec2){
 	if(rec1.height>rec2.height && rec1.width>rec2.width){
     return {top: rec1.top, left:rec1.left, height: rec1.height, width: rec1.width, children:[{
-        top: Math.abs(rec1.top - rec2.top), left: Math.abs(rec1.left-rec2.left), height:rec2.height, width:rec2.width,children[]}]}
+        top: Math.abs(rec1.top - rec2.top), left: Math.abs(rec1.left-rec2.left), height:rec2.height, width:rec2.width,children:[]}]}}
 	else if(rec1.top+rec1.height == rec2.top+rec2.height && rec1.left+rec2.width == rec2.left+rec2.height && rec1.top==rec2.top && rec1.left==rec2.left)
 		return {top: rec2.top, left: rec2.left, height: rec2.height, width: rec2.width, children:[{top:rec1.top,left:rec1.left,height:rec1.height,width:rec1.width, children:[]}]};
 	else
 		return {top: rec1.top, left: rec1.left, height: rec1.height, width: rec1.width, children:[]}
 }
-
 module.exports = updateStructure;
